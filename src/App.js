5258563def21436390f897f5252d7156cecee9c0
jjulissa <julissajoseph9@gmail.com>
2023-02-10 11:39:42 +0100

    install Tailwindcss

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className='text-4xl text-pink-500'>Hola</h1>
+      <h1 className='text-4xl text-pink-500'>Probando Si Funciona Bien el Tailwindcss</h1>
     </div>
   );
 }
