598bd516a745c32ce51c542dedb530753f0c0388
jjulissa <julissajoseph9@gmail.com>
2023-02-13 15:39:04 +0100

    Logo Arreglado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+// import {BrowserRouter, Routes, Route} from 'react-router-dom';  
 import './App.css'; 
-import Logo from "./Components/Logo/Logo"
+
+import Logo from "./Components/Logo/Logo" 
+// import NavButton from "./Components/NavButton/NavButton"; 
+
+
 
 function App() {
   return (
     <div className="App">
-      <Logo />
+      <Logo /> 
+      
     </div>
   );
 }
