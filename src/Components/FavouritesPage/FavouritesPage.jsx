390d4fab1ceab41127dfb8cf62096bdd4545e520
Martin Balverde <tinchoabalverde@gmail.com>
2023-02-14 17:49:04 +0100

    Se agrega el mapa a map Page

--- src/Components/FavouritesPage/FavouritesPage.jsx
+++ src/Components/FavouritesPage/FavouritesPage.jsx
@@ -1,7 +1,7 @@
 export default function FavouritesPage() { 
     return ( 
         <> 
-           
+           <p>afdgdsf</p>
         </>
     )
 }
--- src/Components/MapPage/MapPage.jsx
+++ src/Components/MapPage/MapPage.jsx
@@ -1,7 +1,16 @@
-export default function MapPage() { 
-    return ( 
-        <> 
-           
-        </>
-    )
-}
+import { MapContainer, TileLayer } from 'react-leaflet'
+
+export default function MapPage() {
+
+  return (
+    <>
+      <MapContainer className='w-[80%] h-[500px]' center={[40.6322331, -3.2449314]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+       
+      </MapContainer>
+    </>
+  )
+} 
