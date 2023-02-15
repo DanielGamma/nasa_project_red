fba739e50479a2baa1c63c73d9360b262a9ce6f8
Martin Balverde <tinchoabalverde@gmail.com>
2023-02-15 17:34:07 +0100

    Se añade mapa a seccion map con los fireballs

--- src/Components/MapPage/MapPage.jsx
+++ src/Components/MapPage/MapPage.jsx
@@ -1,16 +1,38 @@
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { useState, useEffect } from "react";
 
 export default function MapPage() {
 
+  const [meteoritos, setMeteoritos] = useState([]);
+
+  useEffect(() => {
+    fetch("https://nasa-api-us5g.onrender.com/api/landings")
+      .then(res => res.json())
+      .then(data => {
+        setMeteoritos(data.landings.filter(fireball => fireball.reclat !== undefined || fireball.reclong !== undefined))
+      })
+      
+    }, []);
+
   return (
-    <>
-      <MapContainer className='w-[80%] h-[500px]' center={[40.6322331, -3.2449314]} zoom={13} scrollWheelZoom={false}>
+    <div className="flex justify-center align-center p-10 bg-baghome h-screen">
+      <MapContainer className='w-full h-[600px]' center={[40.6322331, -3.2449314]} zoom={13} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-       
+         {
+          meteoritos.map((e,i)=> {
+            return (
+            <Marker position={[e.reclat, e.reclong]} key={i}>
+              <Popup>
+                {e.name}
+              </Popup>
+            </Marker>
+            )
+          })
+        } 
       </MapContainer>
-    </>
+    </div>
   )
 } 
